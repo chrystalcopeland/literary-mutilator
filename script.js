68e09c61fcf980f1eb99530e30e63fbf21a6ac57
Chrystal Copeland <canderson73@cnm.edu>
2019-08-13 07:57:06 -0600

    tried to add more javascript

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 window.onload = function () {
-
+	initParagraph2();
+};
+	function initParagraph2 (){
 	let paragraph2 = document.getElementById("paragraph2");
 
-// Turn paragraph purple on mouseover
-	paragraph2.addEventListener("mouseenter", function(event) {
+// change color of paragraph on mouseover
+	paragraph2.addEventListener("mouseover", function(event) {
 		// highlight the mouseenter target
 		event.target.style.color = "purple";
+		// reset the color after a short delay
+		setTimeout(function() {
+			event.target.style.color = "";
+		}, 500);
 	}, false);
 }
